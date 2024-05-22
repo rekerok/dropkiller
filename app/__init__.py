c9e57d67a1995a8a98c601b48596874fc2ddeca8
rekerok05 <rekerok05@gmail.com>
2024-05-22 18:37:02 +0300

    add transfer api

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.api.gas import Gas
 from app.api.balance import Balance
 from app.api.swap import Swap
 from app.api.token import Token
+from app.api.transfer import Transfer
 
 
 app = Flask(__name__)
@@ -22,3 +23,4 @@ api.add_resource(Balance, "/api/balance", endpoint="balance")
 api.add_resource(Token, "/api/token", endpoint="token")
 api.add_resource(Swap, "/api/swap", endpoint="swap")
 api.add_resource(Approve, "/api/approve", endpoint="approve")
+api.add_resource(Transfer, "/api/transfer", endpoint="transfer")
